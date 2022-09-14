df0eaa3ddaaf0f6da44503bbfa702e185a25f246
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2022-09-14 18:33:18 +0200

    ➕ ADD: Frontend Project

---
+++ Frontend/Models/Category.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using System.Text.Json.Serialization;
+
+namespace Frontend.Models;
+
+public class Category
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+
+    public Category(string name)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+    }
+
+    [JsonConstructor]
+    public Category(Guid id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public List<Category> ToList()
+    {
+        return new List<Category> { this };
+    }
+}
+
+public class CategoryValidator : AbstractValidator<Category>
+{
+    public CategoryValidator()
+    {
+        RuleFor(category => category.Name).NotEmpty();
+    }
+}
---
+++ Frontend/Models/Recipe.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json.Serialization;
+using FluentValidation;
+
+namespace Frontend.Models;
+
+public class Recipe
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public List<string> Ingredients { get; set; }
+    public List<string> Instructions { get; set; }
+    public List<Guid> CategoriesIds { get; set; }
+
+    public Recipe(string name, List<string> ingredients, List<string> instructions, List<Guid> categoriesIds)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Ingredients = ingredients;
+        Instructions = instructions;
+        CategoriesIds = categoriesIds;
+    }
+
+    [JsonConstructor]
+    public Recipe(Guid id, string name, List<string> ingredients, List<string> instructions, List<Guid> categoriesIds)
+    {
+        Id = id;
+        Name = name;
+        Ingredients = ingredients;
+        Instructions = instructions;
+        CategoriesIds = categoriesIds;
+    }
+
+    public List<Recipe> ToList()
+    {
+        return new List<Recipe> { this };
+    }
+    }
+
+    public class RecipeValidator : AbstractValidator<Recipe>
+    {
+    public RecipeValidator()
+    {
+        RuleFor(recipe => recipe.Name).NotEmpty();
+        RuleFor(recipe => recipe.Ingredients).NotEmpty();
+        RuleFor(recipe => recipe.Instructions).NotEmpty();
+        RuleFor(recipe => recipe.CategoriesIds).NotEmpty();
+    }
+}
